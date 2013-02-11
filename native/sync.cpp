8abe4876f0842774fafea73ca3df88362678ca17
Daniel Danilatos <daniel@helixta.com.au>
2013-02-11 23:53:24 +1100

    further progress (unfinished)

--- native/sync.cpp
+++ native/sync.cpp
@@ -40,25 +40,41 @@ void Sync::copyBucketVals(Spectrum &spectrum, complex<float> *out) {
 }
 
 float Sync::detectMatch(complex<float> *bucketVals) {
+  // Computes dot-product of normalized bucketVals with normalized bit pattern.
+
   float bucketSum = 0.0;
   float result = 0;
+
   for (int i = 0; i < cfg->numchans; i++) {
     float a = abs(bucketVals[i]);
-    bucketSum += a;
+    // Keep track of sum of squares, to normalise at end
+    bucketSum += a*a;
+
+    // Sync match bit pattern is 1,0,1,0,1,0...
+    // Therefore add every 2nd one
     if (i % 2 == 0) {
       result += a;
     }
   }
+  // Normalize
   result /= sqrt(bucketSum);
   result /= bitPatternAbs;
+  result *= result;
 
+  // Just in case
   result *= 0.999;
   result += 0.0005;
 
-  result = result * 2 - 1;
+  result = acos(result) / (3.14159/2);
 
-  cerr << result << ' ';
-  assert(result > -1.0 && result < 1.0);
+  // Map to range -1,1
+  //result = result * 2 - 1;
+
+ // if (abs(result) > 0.5) {
+ //   cerr << "             ";
+ // }
+  //cerr << result << '\n';
+  //assert(result > -1.0 && result < 1.0);
 
   return result;
 }
@@ -69,8 +85,7 @@ vector<float>::iterator Sync::receiveAudioAndSync(vector<float> &samples) {
   buffer.insert(buffer.end(), samples.begin(), samples.end());
 
   while (buffer.size() >= bufferStart() + cfg->chipSize) {
-
-    Spectrum spectrum(samples.data() + bufferStart(), SAMPLE_RATE, cfg->chipSize);
+    Spectrum spectrum(buffer.data() + bufferStart(), SAMPLE_RATE, cfg->chipSize);
     complex<float> bucketVals[cfg->numchans];
 
     copyBucketVals(spectrum, bucketVals);
@@ -79,7 +94,14 @@ vector<float>::iterator Sync::receiveAudioAndSync(vector<float> &samples) {
     fftSampleIndex++;
   }
 
-  
+  float tot = 0;
+  // WTF compiler errors
+  //for (vector<float>::iterator it = metaBuffer.begin(); it != metaBuffer.end(); ++it) {
+  for (int i = 0; i < metaBuffer.size(); i++) {
+    tot += metaBuffer[i];
+  }
+
+  cerr << (tot / metaBuffer.size()) << '\n';
 
   // Look for sync...
 
